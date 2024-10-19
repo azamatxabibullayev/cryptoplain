032b0fec439af38277e5fd94fef94b297f4756ba
azamatxabibullayev <azamatxabibullayev02@gmail.com>
2024-10-19 12:53:31 +0500

    48th

--- main/forms.py
+++ main/forms.py
@@ -2,11 +2,12 @@ from django import forms
 from .models import Note
 from django.utils.translation import gettext_lazy as _
 
+
 class NoteForm(forms.ModelForm):
     class Meta:
         model = Note
         fields = ['title', 'content']
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': _('Yangi zametka qoshish'), 'class': 'note-name-input'}),
+            'title': forms.TextInput(attrs={'placeholder': _('Zametka nomi'), 'class': 'note-name-input'}),
             'content': forms.Textarea(attrs={'placeholder': _('Zametka'), 'class': 'note-content'}),
         }
